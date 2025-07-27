a3c6c52b2e23e7b89506f28f158011edec94f037
pavxx02 <pavithra020906@gmail.com>
2025-07-27 19:38:34 +0530

    updated ProductList.jsxist

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -274,16 +274,12 @@ function ProductList() {
                 <div className="tag">
                     <div className="luxury">
                         <img src="https://cdn.pixabay.com/photo/2020/08/05/13/12/eco-5465432_1280.png" alt="" />
-                        <div onClick={() => {
-                            setShowCart(false);
-                            setShowPlants(false);
-                        }} style={{ textDecoration: 'none', cursor: 'pointer' }}>
+                        <a href="/e-plantShopping/" style={{ textDecoration: 'none' }}>
                             <div>
                                 <h3 style={{ color: 'white' }}>Paradise Nursery</h3>
                                 <i style={{ color: 'white' }}>Where Green Meets Serenity</i>
                             </div>
-                        </div>
-
+                        </a>
                     </div>
                 </div>
                 <div style={styleObjUl}>
